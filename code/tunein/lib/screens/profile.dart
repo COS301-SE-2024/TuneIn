581ff363ca47d6b4898c983fc7712896118b4d21
Jaden Moodley <u22528492@tuks.co.za>
2024-06-02 15:27:20 +0200

    Profile Components: -Bio -Favourite Genres

--- code/tunein/lib/screens/profile.dart
+++ code/tunein/lib/screens/profile.dart
@@ -98,10 +98,138 @@ class ProfileScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            NowPlaying()
+            NowPlaying(),
+            SizedBox(height: 20),
+            BioSection(
+              content:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ",
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 32),
+              child: Row(
+                children: const [
+                  Text(
+                    "Favorite Genres",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(child: GenreList(items: const ["Pop", "Hip-Hop", "Jazz", "Classical", "Rock"])),
           ],
         ),
       ),
+    );
+  }
+}
+
+class GenreBubble extends StatelessWidget {
+  final String text;
+
+  const GenreBubble({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.0),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(232, 235, 242, 1),
+        borderRadius: BorderRadius.circular(
+            10), // Half of the height to create a stadium shape
+      ),
+      child: Center(
+        // Center the text vertically and horizontally
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GenreList extends StatelessWidget {
+  final List<String> items;
+
+  const GenreList({Key? key, required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 32, // Set the height of the container
+      child: ListView.builder(
+        padding: EdgeInsets.only(left:27),
+        scrollDirection: Axis.horizontal,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return GenreBubble(text: items[index]);
+        },
+      ),
+    );
+  }
+}
+
+class BioSection extends StatelessWidget {
+  final String content;
+
+  const BioSection({
+    super.key,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 32),
+          child: Row(
+            children: const [
+              Text(
+                "Bio",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 32),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  content,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.start,
+                  softWrap: true,
+                  overflow: TextOverflow.visible,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -174,7 +302,8 @@ class NowPlaying extends StatelessWidget {
                 ),
                 Spacer(), // Add Spacer here
                 Padding(
-                  padding: const EdgeInsets.only(right: 16.0), // Add padding if needed
+                  padding: const EdgeInsets.only(
+                      right: 16.0), // Add padding if needed
                   child: Text(
                     "3:05",
                     textAlign: TextAlign.end,
