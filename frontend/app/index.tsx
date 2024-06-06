dbee160598c78ad3a90a6e8482d33e41c192ba50
Linda Masia <masialr3x@gmail.com>
2024-06-06 09:14:08 +0200

    added Tailwindcss

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 import { Link } from "expo-router";
 import RoomCardWidget from './components/RoomCardWidget';
 import { Room } from './components/models/Room';
@@ -26,7 +26,8 @@ const Home: React.FC = () => {
       userProfile: 'https://w7.pngwing.com/pngs/205/731/png-transparent-default-avatar-thumbnail.png',
       username: 'User123',
       tags: ['Tag1', 'Tag2', 'Tag3'],
-    }
+    },
+    // Add more sampleRoomCards...
   ];
 
   const renderItem = ({ item }: { item: Room }) => (
@@ -35,41 +36,19 @@ const Home: React.FC = () => {
         pathname: '/screens/RoomPage',
         params: { room: JSON.stringify(item) },
       }}
-      style={styles.link}
     >
       <RoomCardWidget roomCard={item} />
     </Link>
   );
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Text style={styles.title}>Recent Rooms</Text>
-      <AppCarousel data={sampleRoomCards} renderItem={renderItem}/>
-      <Text style={styles.title}>Picks for you</Text>
-      <AppCarousel data={sampleRoomCards} renderItem={renderItem}/>
+    <View className="flex-1 justify-center pt-4">
+      <Text className="text-2xl font-bold text-gray-800 mt-2 mb-2 ml-8">Recent Rooms</Text>
+      <AppCarousel data={sampleRoomCards} renderItem={renderItem} />
+      <Text className="text-2xl font-bold text-gray-800 mb-2 ml-8">Picks for you</Text>
+      <AppCarousel data={sampleRoomCards} renderItem={renderItem} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#f5f5f5',
-    paddingTop: StatusBar.currentHeight || 0,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    color: '#333',
-    marginTop: 20,
-    marginBottom: 10,
-    marginLeft: 30
-  },
-  link: {
-    marginBottom: 10,
-  }
-});
 
 export default Home;
