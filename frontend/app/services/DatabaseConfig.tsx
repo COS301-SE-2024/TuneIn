021b2c409454b97f2b83d706d29e895ef6472af7
8xbbie <u22609254@tuks.co.za>
2024-06-08 23:06:21 +0200

    stash

--- frontend/app/services/DatabaseConfig.tsx
+++ frontend/app/services/DatabaseConfig.tsx
@@ -1,15 +1,18 @@
-// connect to aws rds and write a postgresql query to insert the user into the database
-// import rds from 'rds';
 import pg from 'pg';
-const { Pool } = pg;
 
-const pool = new Pool({
-  user: 'postgres',
+
+const connection = {
   host: 'tunein-db.che0om0o6j54.af-south-1.rds.amazonaws.com',
+  port: 5432,
   database: 'tunein-db',
-  password: 'P6ppdnhCmw4nG8kU2a1K',
-  port: '5432',
-});
+  user: 'postgres',
+  password: 'P6ppdnhCmw4nG8kU2a1K'
+};
 
-// console.log('Connected to the database', pool);
+const pool = new pg.Pool(connection);
+console.log('Connected to the database', pool);
+pool.query(`SELECT * FROM users`, (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 export default pool;
