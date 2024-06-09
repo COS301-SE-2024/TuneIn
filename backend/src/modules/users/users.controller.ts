dca7e778c4c6486be5c2846c48eec845ce58513e
lkekana <theoriginalles@gmail.com>
2024-06-09 21:09:29 +0200

    defined paths for /users

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -12,11 +12,55 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { ApiTags } from "@nestjs/swagger";
 
+/*
+### `/user` (not demo 1)
+#### GET: gets user info
+no input
+response: return UserDto
+#### PUT or PATCH: user profile info
+input: UserDto
+response: return updated UserDto
+
+### `/user/rooms`
+related to a user's own rooms
+#### GET: get a user's rooms
+no input
+response: an array of RoomDto
+#### POST: create a new room
+input: partial RoomDto
+response: final RoomDto for room (including new id)
+
+### `/user/rooms/recent`
+#### GET: get user's recent rooms
+no input
+response: an array of RoomDto
+
+### `/user/rooms/foryou`
+#### GET: get user's recommended rooms
+no input
+response: an array of RoomDto
+
+### `/user/friends`
+#### GET: get user's friends
+no input
+response: an array of ProfileDto
+
+### `/user/followers`
+#### GET: get list of followers
+no input
+response: an array of ProfileDto
+### `/users/following`
+#### GET: get list of people user is following
+no input
+response: an array of ProfileDto
+*/
 @ApiTags("users")
 @Controller("users")
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
+	//basic CRUD operations on the users table
+	/*
 	@Post()
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.usersService.create(createUserDto);
@@ -42,5 +86,60 @@ export class UsersController {
 	@Delete(":id")
 	remove(@Param("id") id: string) {
 		return this.usersService.remove(id);
+	}
+  */
+
+	@Get()
+	@ApiTags("users")
+	getUserInfo() {
+		return this.usersService.getUserInfo();
+	}
+
+	@Patch()
+	@ApiTags("users")
+	updateUserProfile(@Body() updateUserDto: UpdateUserDto) {
+		return this.usersService.updateUserProfile(updateUserDto);
+	}
+
+	@Get("rooms")
+	@ApiTags("users")
+	getUserRooms() {
+		return this.usersService.getUserRooms();
+	}
+
+	@Post("rooms")
+	@ApiTags("users")
+	createRoom(@Body() createRoomDto: any) {
+		return this.usersService.createRoom(createRoomDto);
+	}
+
+	@Get("rooms/recent")
+	@ApiTags("users")
+	getRecentRooms() {
+		return this.usersService.getRecentRooms();
+	}
+
+	@Get("rooms/foryou")
+	@ApiTags("users")
+	getRecommendedRooms() {
+		return this.usersService.getRecommendedRooms();
+	}
+
+	@Get("friends")
+	@ApiTags("users")
+	getUserFriends() {
+		return this.usersService.getUserFriends();
+	}
+
+	@Get("followers")
+	@ApiTags("users")
+	getFollowers() {
+		return this.usersService.getFollowers();
+	}
+
+	@Get("following")
+	@ApiTags("users")
+	getFollowing() {
+		return this.usersService.getFollowing();
 	}
 }
