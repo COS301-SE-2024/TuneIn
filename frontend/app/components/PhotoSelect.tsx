d5ebc5e1cde7d5616b33dae3511cd3fbd5fd9d66
Jaden Moodley <u22528492@tuks.co.za>
2024-06-09 20:50:39 +0200

    Edited PhotoSelect Component

--- frontend/app/components/PhotoSelect.tsx
+++ frontend/app/components/PhotoSelect.tsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet, Modal, TouchableOpacity, Button } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import {
+	View,
+	Text,
+	Image,
+	StyleSheet,
+	Modal,
+	TouchableOpacity,
+	Button,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker"; // Import Expo's image picker library
 
 const PhotoSelect = ({ isVisible, onClose, onImageUpload }) => {
 	const [image, setImage] = useState(null);
-    const [visibility, setVisibility] = useState(isVisible)
+	const [visibility, setVisibility] = useState(isVisible);
 
-    useEffect(() => {
-        setVisibility(isVisible);
-    }, [isVisible]);
+	useEffect(() => {
+		setVisibility(isVisible);
+	}, [isVisible]);
 
 	const pickImage = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
@@ -21,10 +29,11 @@ const PhotoSelect = ({ isVisible, onClose, onImageUpload }) => {
 
 		if (!result.canceled) {
 			setImage(result["uri"]);
-			onImageUpload(result["uri"]);		}
+			onImageUpload(result["uri"]);
+		}
 	};
 
-	return (
+    return (
         <Modal
             transparent={true}
             animationType="slide"
@@ -33,10 +42,13 @@ const PhotoSelect = ({ isVisible, onClose, onImageUpload }) => {
         >
             <View style={styles.modalContainer}>
                 <View style={styles.modal}>
-                    <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-                        <Ionicons name="close" size={24} color="black" />
-                    </TouchableOpacity>
-                    <Text style={styles.modalTitle}>Add Image</Text>
+                    <View style={styles.headerContainer}>
+                        
+                        <Text style={styles.modalTitle}>Add Image</Text>
+                        <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                            <Ionicons name="close" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
                     {image && (
                         <View style={styles.imageContainer}>
                             <Image source={{ uri: image }} style={styles.image} />
@@ -47,44 +59,53 @@ const PhotoSelect = ({ isVisible, onClose, onImageUpload }) => {
             </View>
         </Modal>
     );
-};
-
-const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        backgroundColor: "rgba(0,0,0,0.5)",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    modal: {
-        backgroundColor: "white",
-        padding: 20,
-        borderRadius: 10,
-        alignItems: "center",
-    },
-    modalTitle: {
-        fontSize: 18,
-        fontWeight: "bold",
-        marginBottom: 10,
-    },
-    imageContainer: {
-        
-        marginVertical: 10,
-        alignItems: "center",
-    },
-    image: {
-        width: 200,
-        height: 200,
-        resizeMode: "contain",
-    },
-    chooseButton: {
-        marginTop: 20,
-    },
-    closeButton: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
-    },
-});
-
-export default PhotoSelect;
+}
+    
+    const styles = StyleSheet.create({
+        modalContainer: {
+            flex: 1,
+            backgroundColor: "rgba(0,0,0,0.5)",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        modal: {
+            backgroundColor: "white",
+            paddingHorizontal: 10,
+            paddingBottom: 12,
+            borderRadius: 10,
+            alignItems: "center",
+            height: 110,
+        },
+        headerContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingBottom: 20,
+            width: '100%',
+        },
+        closeButton: {
+            paddingVertical: 5,
+        },
+        modalTitle: {
+            fontSize: 18,
+            fontWeight: "bold",
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+        imageContainer: {
+            marginVertical: 10,
+            alignItems: "center",
+        },
+        image: {
+            width: 200,
+            height: 200,
+            resizeMode: "contain",
+        },
+        chooseButton: {
+            marginTop: 20,
+        },
+    });
+    
+    export default PhotoSelect;
+    
+    
