8ea8c968fb959f91b000c5c25116d749594cff91
Linda Masia <masialr3x@gmail.com>
2024-06-09 10:26:32 +0200

    tailwind Register page
    redid register page in tailwind

--- frontend/app/services/DatabaseConfig.tsx
+++ frontend/app/services/DatabaseConfig.tsx
@@ -1,18 +1,18 @@
-import pg from 'pg';
+// import pg from 'pg';
 
 
-const connection = {
-  host: 'tunein-db.che0om0o6j54.af-south-1.rds.amazonaws.com',
-  port: 5432,
-  database: 'tunein-db',
-  user: 'postgres',
-  password: 'P6ppdnhCmw4nG8kU2a1K'
-};
+// const connection = {
+//   host: 'tunein-db.che0om0o6j54.af-south-1.rds.amazonaws.com',
+//   port: 5432,
+//   database: 'tunein-db',
+//   user: 'postgres',
+//   password: 'P6ppdnhCmw4nG8kU2a1K'
+// };
 
-const pool = new pg.Pool(connection);
-console.log('Connected to the database', pool);
-pool.query(`SELECT * FROM users`, (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-export default pool;
+// const pool = new pg.Pool(connection);
+// console.log('Connected to the database', pool);
+// pool.query(`SELECT * FROM users`, (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+// export default pool;
