ef84dddb5906067c0f5af67c90fdf10f711a6729
lkekana <theoriginalles@gmail.com>
2024-06-09 14:58:31 +0200

    minor logic fixes for Auth

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,80 +1,80 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import * as AWS from "aws-sdk";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
-  private cognitoIdentityServiceProvider: AWS.CognitoIdentityServiceProvider;
-  private userPoolId: string;
-  private clientId: string;
+	private cognitoIdentityServiceProvider: AWS.CognitoIdentityServiceProvider;
+	private userPoolId: string;
+	private clientId: string;
 
-  constructor(private configService: ConfigService) {
-    this.cognitoIdentityServiceProvider =
-      new AWS.CognitoIdentityServiceProvider({
-        region: 'af-south-1',
-        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: this.configService.get<string>(
-          'AWS_SECRET_ACCESS_KEY',
-        ),
-      });
-    this.userPoolId =
-      this.configService.get<string>('AWS_COGNITO_USER_POOL_ID') || '';
-    this.clientId =
-      this.configService.get<string>('AWS_COGNITO_CLIENT_ID') || '';
-  }
+	constructor(private configService: ConfigService) {
+		this.cognitoIdentityServiceProvider =
+			new AWS.CognitoIdentityServiceProvider({
+				region: "af-south-1",
+				accessKeyId: this.configService.get<string>("AWS_ACCESS_KEY_ID"),
+				secretAccessKey: this.configService.get<string>(
+					"AWS_SECRET_ACCESS_KEY",
+				),
+			});
+		this.userPoolId =
+			this.configService.get<string>("AWS_COGNITO_USER_POOL_ID") || "";
+		this.clientId =
+			this.configService.get<string>("AWS_COGNITO_CLIENT_ID") || "";
+	}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    if (!this.userPoolId || !this.clientId) {
-      throw new Error('Missing Cognito configuration');
-    }
+	async validateUser(username: string, password: string): Promise<any> {
+		if (!this.userPoolId || !this.clientId) {
+			throw new Error("Missing Cognito configuration");
+		}
 
-    const params: AWS.CognitoIdentityServiceProvider.AdminInitiateAuthRequest =
-      {
-        AuthFlow: 'ADMIN_NO_SRP_AUTH',
-        ClientId: this.clientId,
-        UserPoolId: this.userPoolId,
-        AuthParameters: {
-          USERNAME: username,
-          PASSWORD: password,
-        },
-      };
+		const params: AWS.CognitoIdentityServiceProvider.AdminInitiateAuthRequest =
+			{
+				AuthFlow: "ADMIN_NO_SRP_AUTH",
+				ClientId: this.clientId,
+				UserPoolId: this.userPoolId,
+				AuthParameters: {
+					USERNAME: username,
+					PASSWORD: password,
+				},
+			};
 
-    try {
-      const authResponse = await this.cognitoIdentityServiceProvider
-        .adminInitiateAuth(params)
-        .promise();
-      if (authResponse.AuthenticationResult) {
-        const user = await this.cognitoIdentityServiceProvider
-          .adminGetUser({
-            UserPoolId: this.userPoolId,
-            Username: username,
-          })
-          .promise();
+		try {
+			const authResponse = await this.cognitoIdentityServiceProvider
+				.adminInitiateAuth(params)
+				.promise();
+			if (authResponse.AuthenticationResult) {
+				const user = await this.cognitoIdentityServiceProvider
+					.adminGetUser({
+						UserPoolId: this.userPoolId,
+						Username: username,
+					})
+					.promise();
 
-        return {
-          username: user.Username,
-          attributes: user.UserAttributes,
-          authenticationResult: authResponse.AuthenticationResult,
-        };
-      }
-    } catch (error) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-  }
+				return {
+					username: user.Username,
+					attributes: user.UserAttributes,
+					authenticationResult: authResponse.AuthenticationResult,
+				};
+			}
+		} catch (error) {
+			throw new UnauthorizedException("Invalid credentials");
+		}
+	}
 
-  // eslint-disable-next-line prettier/prettier
+	// eslint-disable-next-line prettier/prettier
   async listUsers(userSub: string): Promise<AWS.CognitoIdentityServiceProvider.ListUsersResponse> {
-    const params = {
-      UserPoolId: this.userPoolId,
-    };
+		const params = {
+			UserPoolId: this.userPoolId,
+		};
 
-    try {
-      const response = await this.cognitoIdentityServiceProvider
-        .listUsers(params)
-        .promise();
-      return response;
-    } catch (error) {
-      throw new UnauthorizedException('Error listing users');
-    }
-  }
+		try {
+			const response = await this.cognitoIdentityServiceProvider
+				.listUsers(params)
+				.promise();
+			return response;
+		} catch (error) {
+			throw new UnauthorizedException("Error listing users");
+		}
+	}
 }
