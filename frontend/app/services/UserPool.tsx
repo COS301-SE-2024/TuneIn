38352014027d783849c3017c178798c8f18c9c03
Linda Masia <masialr3x@gmail.com>
2024-06-09 22:38:00 +0200

    fixed .env issue

--- frontend/app/services/UserPool.tsx
+++ frontend/app/services/UserPool.tsx
@@ -1,11 +1,9 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { EXPO_USER_POOL_ID, EXPO_CLIENT_ID } from "@env";
 
 const poolData = {
-  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
-  ClientId: process.env.REACT_APP_CLIENT_ID,
+  UserPoolId: EXPO_USER_POOL_ID,
+  ClientId: EXPO_CLIENT_ID,
 };
 
 export default new CognitoUserPool(poolData);
