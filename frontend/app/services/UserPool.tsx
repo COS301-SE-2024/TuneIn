399d2d8c914f0acd0172535e460016bc9fada7ce
8xbbie <u22609254@tuks.co.za>
2024-06-10 17:12:40 +0200

    Update UserPool.tsx

--- frontend/app/services/UserPool.tsx
+++ frontend/app/services/UserPool.tsx
@@ -5,7 +5,5 @@ const poolData = {
   UserPoolId: EXPO_USER_POOL_ID,
   ClientId: EXPO_CLIENT_ID,
 };
-console.log(EXPO_CLIENT_ID, EXPO_USER_POOL_ID);
 
-console.log("UserPool.tsx: poolData", poolData);
 export default new CognitoUserPool(poolData);
