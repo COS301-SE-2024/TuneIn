9ebe631969e2fb327f009ddbde7c6bd62513cced
lkekana <theoriginalles@gmail.com>
2024-06-15 23:56:10 +0200

    add all planned socket events to chat gateway

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -6,6 +6,7 @@ import {
 	WebSocketServer,
 } from "@nestjs/websockets";
 import { Server } from "socket.io";
+import { SOCKET_EVENTS } from "src/config/constants";
 
 @WebSocketGateway({
 	namespace: "/chat",
@@ -27,9 +28,65 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 	}
 
 	@SubscribeMessage("message")
-	handleMessage(client: any, payload: any): string {
+	handleMessage(client: any, payload: any): void {
 		console.log(payload);
 		//Hello World
 		this.server.emit("message", { response: "Hello World" });
 	}
+
+  @SubscribeMessage(SOCKET_EVENTS.CONNECTION)
+  handleJoinRoom(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.DISCONNECT)
+  handleLeaveRoom(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.LIVE_MESSAGE)
+  handleLiveMessage(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.GET_LIVE_CHAT_HISTORY)
+  handleGetLiveChatHistory(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.DIRECT_MESSAGE)
+  handleDirectMessage(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.GET_DIRECT_MESSAGE_HISTORY)
+  handleGetDirectMessageHistory(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.TYPING)
+  handleTyping(client: any, payload: any): void {
+    //this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.STOP_TYPING)
+  handleStopTyping(client: any, payload: any): void {
+	//this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.ERROR)
+  handleError(client: any, payload: any): void {
+	//this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.JOIN_ROOM)
+  handleJoinRoom(client: any, payload: any): void {
+	//this.server.emit();
+  }
+
+  @SubscribeMessage(SOCKET_EVENTS.LEAVE_ROOM)
+  handleLeaveRoom(client: any, payload: any): void {
+	//this.server.emit();
+  }
+
 }
