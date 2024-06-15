396f51ac04a196ff057638f6784ea2c71f0b4c5b
lkekana <theoriginalles@gmail.com>
2024-06-15 22:59:43 +0200

    seems to work correctly in Postman😤

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,9 +1,35 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
+} from "@nestjs/websockets";
+import { Server } from "socket.io";
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+@WebSocketGateway({
+	namespace: "/chat",
+	transports: ["websocket"],
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+	@WebSocketServer() server: Server;
+
+	handleConnection(client: any, ...args: any[]) {
+		console.log("Client connected");
+	}
+
+	handleDisconnect(client: any) {
+		console.log("Client disconnected");
+	}
+
+	@SubscribeMessage("message")
+	handleMessage(client: any, payload: any): string {
+		console.log(payload);
+		//Hello World
+		this.server.emit("message", { response: "Hello World" });
+	}
 }
