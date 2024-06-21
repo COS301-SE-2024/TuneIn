d19001422b940b9983beb0d32fc08dfc6630ac90
8xbbie <u22609254@tuks.co.za>
2024-06-22 00:42:20 +0200

    added room endpoints

--- backend/src/modules/rooms/rooms.controller.ts
+++ backend/src/modules/rooms/rooms.controller.ts
@@ -116,7 +116,7 @@ export class RoomsController {
 		return this.roomsService.updateRoom(roomID, updateRoomDto);
 	}
 
-	/*
+		/*
     DELETE /rooms/{roomID}
     deletes the room (only if it belongs to the user)
     no input
@@ -125,8 +125,23 @@ export class RoomsController {
 	@UseGuards(JwtAuthGuard)
 	@Delete(":roomID")
 	@ApiTags("rooms")
-	deleteRoom(@Request() req: any, @Param("roomID") roomID: string): boolean {
-		return this.roomsService.deleteRoom(roomID);
+	@ApiOkResponse({
+		description: "Room deleted successfully.",
+		type: Boolean,
+	})
+	@ApiBadRequestResponse({
+		description: "User is not the creator of the room.",
+		type: Boolean,
+	})
+	@ApiParam({ name: "roomID", required: true })
+	@ApiBearerAuth()
+	@ApiOperation({ summary: "Delete a room" })
+	async deleteRoom(@Request() req: any, @Param("roomID") roomID: string): Promise<boolean> {
+		// check using jwt token whether the user is the creator of the room
+		// if not, return 403
+		// if yes, delete the room and return 200
+		const userInfo: JWTPayload = this.auth.getUserInfo(req);
+		return await this.roomsService.deleteRoom(roomID, userInfo.id);
 	}
 
 	/*
@@ -135,11 +150,23 @@ export class RoomsController {
     no input
     response: (2xx for success, 4xx for error)
     */
+   	// @ApiBearerAuth()
 	@UseGuards(JwtAuthGuard)
 	@Post(":roomID/join")
 	@ApiTags("rooms")
-	joinRoom(@Request() req: any, @Param("roomID") roomID: string): boolean {
-		return this.roomsService.joinRoom(roomID);
+	@ApiOkResponse({
+		description: "User joined room successfully.",
+		type: Boolean,
+	})
+	@ApiOperation({ summary: "Join a room"})
+	@ApiBadRequestResponse({ 
+		description: "User already in room.",
+		type: Boolean,
+	})
+	@ApiParam({ name: "roomID", required: true })
+	async joinRoom(@Request() req: any, @Param("roomID") roomID: string): Promise<boolean> {
+		const userID: JWTPayload = this.auth.getUserInfo(req);
+		return await this.roomsService.joinRoom(roomID, userID.id)
 	}
 
 	/*
@@ -148,11 +175,23 @@ export class RoomsController {
     no input
     response: (2xx for success, 4xx for error)
     */
+	@ApiBearerAuth()
 	@UseGuards(JwtAuthGuard)
 	@Post(":roomID/leave")
 	@ApiTags("rooms")
-	leaveRoom(@Request() req: any, @Param("roomID") roomID: string): boolean {
-		return this.roomsService.leaveRoom(roomID);
+	@ApiOkResponse({
+		description: "User left room successfully.",
+		type: Boolean,
+	})
+	@ApiOperation({ summary: "Leave a room" })
+	@ApiBadRequestResponse({
+		description: "User not in room.",
+		type: Boolean,
+	})
+	@ApiParam({ name: "roomID", required: true })
+	async leaveRoom(@Request() req: any, @Param("roomID") roomID: string): Promise<boolean> {
+		const userID: JWTPayload = this.auth.getUserInfo(req);
+		return await this.roomsService.leaveRoom(roomID, userID.id);
 	}
 
 	/*
@@ -161,46 +200,19 @@ export class RoomsController {
     no input
     response: array of ProfileDto
     */
+	@ApiBearerAuth()
 	@UseGuards(JwtAuthGuard)
 	@Get(":roomID/users")
 	@ApiTags("rooms")
-	getRoomUsers(
-		@Request() req: any,
-		@Param("roomID") roomID: string,
-	): UserProfileDto[] {
-		return this.roomsService.getRoomUsers(roomID);
-	}
-
-	/*
-    GET /rooms/{roomID}/songs
-    returns the queue
-    no input
-    response: array of SongInfoDto
-    */
-	@UseGuards(JwtAuthGuard)
-	@Get(":roomID/songs")
-	@ApiTags("rooms")
-	getRoomQueue(
-		@Request() req: any,
-		@Param("roomID") roomID: string,
-	): SongInfoDto[] {
-		return this.roomsService.getRoomQueue(roomID);
-	}
-
-	/*
-    DELETE /rooms/{roomID}/songs
-    clears the queue (except for current song, if playing)
-    no input
-    response: (2xx for success, 4xx for error)
-    */
-	@UseGuards(JwtAuthGuard)
-	@Delete(":roomID/songs")
-	@ApiTags("rooms")
-	clearRoomQueue(
-		@Request() req: any,
-		@Param("roomID") roomID: string,
-	): boolean {
-		return this.roomsService.clearRoomQueue(roomID);
+	@ApiOkResponse({
+		description: "The users in the room as an array of UserProfileDto.",
+		type: UserProfileDto,
+		isArray: true,
+	})
+	@ApiOperation({ summary: "Get users in a room" })
+	@ApiParam({ name: "roomID", required: true })
+	async getRoomUsers(@Request() req: any, @Param("roomID") roomID: string): Promise<UserProfileDto[]> {
+		return await this.roomsService.getRoomUsers(roomID);
 	}
 
 	/*
--- backend/src/modules/rooms/rooms.service.ts
+++ backend/src/modules/rooms/rooms.service.ts
@@ -79,24 +79,129 @@ export class RoomsService {
 		return new RoomDto();
 	}
 
-	deleteRoom(roomID: string): boolean {
-		// TODO: Implement logic to delete room
-		return false;
+	async deleteRoom(room_id: string, room_creator: string): Promise<boolean> {
+		// Check if the room exists
+		// delete the room user is the owner
+		var isDeleted = false;
+		try {
+			await this.prisma.room.delete({
+				where: { room_id, room_creator }
+			}).then((room) => {
+				// console.log("is deleting", room);
+				isDeleted = true;
+			}
+			);
+			// console.log(isDeleted);
+			return isDeleted;
+		} catch (error) {
+			return false;
+		}
+	  }
+
+	async joinRoom(room_id: string, user_id: string): Promise<boolean> {
+		console.log("user", user_id, "joining room", room_id);
+		try {
+			// Check if the user is already in the room
+			const room = await this.prisma.participate.findFirst({
+				where: {
+					room_id: room_id,
+					user_id: user_id
+				}
+			})
+	
+			// If the user is already in the room, return false
+			if (room !== null) {
+				return false;
+			}
+	
+			// Add the user to the room
+			await this.prisma.participate.create({
+				data: {
+					room_id: room_id,
+					user_id: user_id
+				}
+			});
+	
+			return true;
+		} catch (error) {
+			console.error("Error joining room:");
+			return false;
+		}
 	}
 
-	joinRoom(roomID: string): boolean {
-		// TODO: Implement logic to join room
-		return false;
-	}
-
-	leaveRoom(roomID: string): boolean {
+	async leaveRoom(room_id: string, user_id: string): Promise<boolean> {
 		// TODO: Implement logic to leave room
-		return false;
+		console.log("user", user_id, "leaving room", room_id);
+		try {
+			// Check if the user is already in the room
+			const room = await this.prisma.participate.findFirst({
+				where: {
+					room_id: room_id,
+					user_id: user_id
+				}
+			})
+	
+			// If the user is already in the room, return false
+			if (room === null) {
+				return false;
+			}
+	
+			// Add the user to the room
+			await this.prisma.participate.delete({
+				where: {
+					participate_id: room.participate_id
+				}
+			});
+	
+			return true;
+		} catch (error) {
+			console.error("Error leaving room:");
+			return false;
+		}
 	}
 
-	getRoomUsers(roomID: string): UserProfileDto[] {
-		// TODO: Implement logic to get room users
-		return [];
+	async getNumFollowers(user_id: string, getFollowers: boolean): Promise<number> {
+		try {
+			const _where: object = getFollowers? {
+				follower: user_id
+			} : {
+				followee: user_id
+			};
+			const followers: number = await this.prisma.follows.count({
+				where: _where
+			});
+			return followers;
+		} catch(error) {
+			return 0;
+		}
+	}
+
+	async getRoomUsers(room_id: string): Promise<UserProfileDto[]> {
+		try {
+			// write a query to get all the users in the room
+			const users = await this.prisma.participate.findMany({
+				where: {
+					room_id: room_id
+				},
+				include: {
+					users: true
+				}
+			});
+			// map all the users to the userprofiledto
+			console.log("Users in room", users);
+			const userProfiles: (UserProfileDto | null)[] = await Promise.all(users.map(async (user) => {
+				const userProfile = await this.dtogen.generateUserProfileDto(user.users.user_id);
+				return userProfile;
+			}));
+	
+			// filter out null values
+			const filteredUserProfiles: UserProfileDto[] = userProfiles.filter((userProfile) => userProfile !== null) as UserProfileDto[];
+			console.log("Filtered user profiles", filteredUserProfiles);
+			return filteredUserProfiles;
+		} catch (error) {
+			console.error("Error getting room users:", error);
+			return [];
+		}
 	}
 
 	getRoomQueue(roomID: string): SongInfoDto[] {
