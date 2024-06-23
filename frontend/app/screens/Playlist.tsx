252a83d5eb61b4067d496c60f73b81b3b764d41c
Linda Masia <masialr3x@gmail.com>
2024-06-23 13:11:59 +0200

    set up global player context

--- frontend/app/screens/Playlist.tsx
+++ frontend/app/screens/Playlist.tsx
@@ -15,35 +15,32 @@ interface Track {
 
 const Playlist = () => {
   const router = useRouter();
-  const { queue, currentTrackIndex,Room_id,mine } = useLocalSearchParams();
-  console.log("playlist owner: ", mine);
+  const { queue, currentTrackIndex, Room_id, mine } = useLocalSearchParams();
   const isMine = mine === "true";
-  // Ensure queue is correctly typed as an array of Track objects
   const [playlist, setPlaylist] = useState<Track[]>([]);
 
   useEffect(() => {
     if (typeof queue === 'string') {
-      // Assuming queue is a JSON string, parse it into an array of Track objects
       const parsedQueue = JSON.parse(queue) as Track[];
       setPlaylist(parsedQueue);
     } else if (Array.isArray(queue)) {
-      // If queue is already an array (for testing purposes or other scenarios)
       setPlaylist(queue);
     }
   }, [queue]);
 
   useEffect(() => {
-    console.log('Current Track Index:', currentTrackIndex);
+    console.log('Current Track Index:', Number(currentTrackIndex));
   }, [currentTrackIndex]);
+  
 
   const navigateToAddSong = () => {
-
     router.navigate({
       pathname: "/screens/rooms/EditPlaylist",
-      params: { queue: queue,
+      params: {
+        queue: queue,
         currentTrackIndex: currentTrackIndex,
         Room_id: Room_id,
-       },
+      },
     });
   };
 
@@ -53,32 +50,32 @@ const Playlist = () => {
         <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
           <Ionicons name="chevron-back" size={24} color="black" />
         </TouchableOpacity>
-        <Text style={styles.pageName}>Playlist</Text>
+        <Text style={styles.pageName}>Queue</Text>
       </View>
       <View style={styles.songListContainer}>
         {playlist.map((track, index) => (
           <SongList
             key={index}
-            songNumber={index + 1} // Assuming index starts from 1
+            songNumber={index + 1}
             songName={track.name}
             artist={track.artistNames}
             albumCoverUrl={track.albumArtUrl}
             voteCount={track.voteCount}
             showVoting={track.showVoting}
-            index={index} // Pass index for swapping (if needed)
-           
+            index={index}
+            isCurrent={index === Number(currentTrackIndex)} // Check if current song
           />
         ))}
       </View>
       {isMine ? (
-      <View style={styles.addButtonContainer}>
-        <TouchableOpacity style={styles.addButton} onPress={navigateToAddSong}>
-          <Text style={styles.addButtonText}>Add Song</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={styles.addButtonContainer}>
+          <TouchableOpacity style={styles.addButton} onPress={navigateToAddSong}>
+            <Text style={styles.addButtonText}>Add Song</Text>
+          </TouchableOpacity>
+        </View>
       ) : (
-				<View></View>
-			)}
+          <View></View>
+        )}
     </View>
   );
 };
