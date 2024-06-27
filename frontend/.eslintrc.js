060bed7b591509488bb5e70302e4c6c62df3478e
lkekana <theoriginalles@gmail.com>
2024-06-27 14:30:14 +0200

    more linting improvements

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,6 +4,7 @@ const { rules } = require("eslint-config-prettier");
 module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier"],
+  ignorePatterns: [".eslintrc.js", "node_modules/", "api-client/", "babel.config.js"],
   rules: {
     // Possible problems
     "no-constructor-return": "error",
