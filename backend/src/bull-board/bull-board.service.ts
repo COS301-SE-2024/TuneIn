ac26e35e9e45d95660d53400b2c6933f4b7c015e
lkekana <theoriginalles@gmail.com>
2024-06-28 13:51:47 +0200

    added a check for hot reloading the BullBoard sub-service

--- backend/src/bull-board/bull-board.service.ts
+++ backend/src/bull-board/bull-board.service.ts
@@ -1,4 +1,3 @@
-// bull-board.ts
 import { createBullBoard } from "@bull-board/api";
 import { BullAdapter } from "@bull-board/api/bullAdapter";
 import { ExpressAdapter } from "@bull-board/express";
@@ -9,9 +8,16 @@ import { Injectable, OnModuleInit } from "@nestjs/common";
 
 @Injectable()
 export class BullBoardService implements OnModuleInit {
+	private static serverStarted = false; // Static property to track server status
+
 	constructor(@InjectQueue("task-queue") private readonly taskQueue: Queue) {}
 
 	onModuleInit() {
+		if (BullBoardService.serverStarted) {
+			console.log("Bull Board server is already running.");
+			return;
+		}
+
 		const serverAdapter = new ExpressAdapter();
 		createBullBoard({
 			queues: [new BullAdapter(this.taskQueue)],
@@ -23,10 +29,15 @@ export class BullBoardService implements OnModuleInit {
 		app.use("/admin/queues", serverAdapter.getRouter());
 
 		const port = 3001;
-		app.listen(port, () => {
-			console.log(
-				`Bull Board is running on http://localhost:${port}/admin/queues`,
-			);
-		});
+		app
+			.listen(port, () => {
+				console.log(
+					`Bull Board is running on http://localhost:${port}/admin/queues`,
+				);
+				BullBoardService.serverStarted = true; // Mark server as started
+			})
+			.on("error", (err) => {
+				console.error(err);
+			});
 	}
 }
