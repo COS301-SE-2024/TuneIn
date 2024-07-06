082a6ee1884881316ee95ee7b7be3af6fcc914f2
lkekana <theoriginalles@gmail.com>
2024-07-06 17:32:29 +0200

    last fix

--- frontend/@env.d.ts
+++ frontend/@env.d.ts
@@ -8,4 +8,5 @@ declare module "@env" {
 	export const AWS_ACCESS_KEY_ID: string;
 	export const AWS_SECRET_ACCESS_KEY: string;
 	export const AWS_S3_BUCKET_NAME: string;
+	export const JWT_SECRET_KEY: string;
 }
