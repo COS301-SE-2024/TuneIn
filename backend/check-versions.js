c59ba435096764bdb4f05b9c6b757f79a73f2386
lkekana <theoriginalles@gmail.com>
2024-07-06 11:43:58 +0200

    fixing linting in backend script & update frontend script

--- backend/check-versions.js
+++ backend/check-versions.js
@@ -1,29 +1,41 @@
-const { execSync } = require('child_process');
-const { engines } = require('./package.json');
+import { execSync } from "child_process";
+import { engines } from "./package.json";
 
 const compareVersions = (currentVersion, requiredVersion) => {
-  const current = currentVersion.split('.').map(Number);
-  const required = requiredVersion.split('.').map(Number);
+	const current = currentVersion.split(".").map(Number);
+	const required = requiredVersion.split(".").map(Number);
 
-  for (let i = 0; i < required.length; i++) {
-    if (current[i] < required[i]) return false;
-    if (current[i] > required[i]) return true;
-  }
-  return true;
+	for (let i = 0; i < required.length; i++) {
+		if (current[i] < required[i]) return false;
+		if (current[i] > required[i]) return true;
+	}
+	return true;
 };
 
 const versionRequirements = [
-  { name: 'node', currentVersion: process.version.replace('v', ''), requiredVersion: engines.node },
-  { name: 'npm', currentVersion: execSync('npm --version').toString().trim(), requiredVersion: engines.npm }
+	{
+		name: "node",
+		currentVersion: process.version.replace("v", ""),
+		requiredVersion: engines.node,
+	},
+	{
+		name: "npm",
+		currentVersion: execSync("npm --version").toString().trim(),
+		requiredVersion: engines.npm,
+	},
 ];
 
 versionRequirements.forEach(({ name, currentVersion, requiredVersion }) => {
-  if (!compareVersions(currentVersion, requiredVersion)) {
-    console.error(`Required ${name} version: ${requiredVersion}, but found: ${currentVersion}. Use \`nvm use stable\` to switch to the correct version.`);
-    process.exit(1);
-  } else {
-    console.log(`Current ${name} version: ${currentVersion} matches the required version: ${requiredVersion}`);
-  }
+	if (!compareVersions(currentVersion, requiredVersion)) {
+		console.error(
+			`Required ${name} version: ${requiredVersion}, but found: ${currentVersion}. Use \`nvm use stable\` to switch to the correct version.`,
+		);
+		process.exit(1);
+	} else {
+		console.log(
+			`Current ${name} version: ${currentVersion} matches the required version: ${requiredVersion}`,
+		);
+	}
 });
 
-console.log('Node and npm versions are as required.');
+console.log("Node and npm versions are as required.");
