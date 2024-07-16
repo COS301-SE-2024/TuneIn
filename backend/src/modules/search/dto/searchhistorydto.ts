75fd8f90a2db67e474ee000e087726fcbdff397b
Jaden Moodley <u22528492@tuks.co.za>
2024-07-16 19:28:56 +0200

    Search History Dto

---
+++ backend/src/modules/search/dto/searchhistorydto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsDateString, IsObject, ValidateNested } from "class-validator";
+import { UserDto } from "../../users/dto/user.dto";
+import { RoomDto } from "../../rooms/dto/room.dto";
+
+export class SearchHistoryDto {
+    @ApiProperty()
+	@IsString()
+	search_term: string;
+
+    @ApiProperty()
+	@IsDateString()
+	search_time: Date;
+
+    @ApiProperty()
+	@IsObject()
+	@ValidateNested()
+	search_result: UserDto | RoomDto;
+}
