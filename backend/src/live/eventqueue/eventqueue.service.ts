c7678bd59c85a3838c86090143b4432a734eac86
lkekana <theoriginalles@gmail.com>
2024-07-18 13:59:30 +0200

    add concurrent event queuing system

--- backend/src/live/eventqueue/eventqueue.service.ts
+++ backend/src/live/eventqueue/eventqueue.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
+import * as async from "async";
+
+export type TaskFunction = () => Promise<void>;
 
 @Injectable()
-export class EventQueueService {}
+export class EventQueueService {
+	private readonly queue: async.QueueObject<TaskFunction>;
+	private readonly logger = new Logger(EventQueueService.name);
+
+	constructor() {
+		this.queue = async.queue(async (task: TaskFunction) => {
+			await task();
+		}, 1); // Concurrency set to 1 to ensure FIFO processing
+
+		this.queue.error((error, task) => {
+			this.logger.error(`Task encountered an error: ${error}`, task.toString());
+		});
+
+		this.queue.drain(() => {
+			this.logger.log("All tasks have been processed.");
+		});
+	}
+
+	addToQueue(task: TaskFunction): void {
+		this.queue.push(task);
+	}
+}
