6844b717558ea63482e2d1432d6abf385830f5f7
Jaden Moodley <u22528492@tuks.co.za>
2024-07-22 12:52:38 +0200

    roomSearch unit tests

--- backend/src/modules/search/search.service.spec.ts
+++ backend/src/modules/search/search.service.spec.ts
@@ -5,6 +5,7 @@ import { prismaMock } from '../../../singleton'
 import { DbUtilsService } from "../db-utils/db-utils.service";
 import { DtoGenService } from "../dto-gen/dto-gen.service";
 import { UserDto } from "../users/dto/user.dto";
+import { RoomDto } from "../rooms/dto/room.dto";
 
 jest.mock("../db-utils/db-utils.service");
 jest.mock("../dto-gen/dto-gen.service");
@@ -112,5 +113,111 @@ describe("searchUsers function", () => {
 		const result = await service.searchUsers("testing");
 
 		expect(result).toMatchObject(uDtoMock);
+	});
+});
+
+describe("searchRooms function", () => {
+	let service: SearchService;
+	let dbUtils: DbUtilsService;
+	let dtoGen: DtoGenService;
+	const roomMock = [
+		{
+		  room_id: '66bb6bf7-25be-45af-bc38-7e7e258797b8',
+		  name: 'chill vibes',
+		  description: 'A room for relaxing and enjoying soothing music. Join us to unwind and chill with your favorite tunes.',
+		  username: 'farmer 345',
+		  distance: 6
+		}
+	  ];
+
+	const rDtoMock = [
+		{
+		  "creator": {
+			"profile_name": "Farmer23",
+			"userID": "01ece2d8-e091-7023-c1f2-d3399faa7071",
+			"username": "farmer 345",
+			"profile_picture_url": "https://tunein-nest-bucket.s3.af-south-1.amazonaws.com/2024-06-23T13:23:20.848Z-image.jpeg",
+			"followers": {
+			  "count": 0,
+			  "data": []
+			},
+			"following": {
+			  "count": 0,
+			  "data": []
+			},
+			"links": {
+			  "count": 0,
+			  "data": []
+			},
+			"bio": "Music enthusiast who loves exploring new tunes across various genres. Always on the lookout for fresh beats and hidden gems!",
+			"current_song": {
+			  "title": "",
+			  "artists": [],
+			  "cover": "",
+			  "start_time": "2024-07-22T10:18:22.486Z"
+			},
+			"fav_genres": {
+			  "count": 0,
+			  "data": []
+			},
+			"fav_songs": {
+			  "count": 0,
+			  "data": []
+			},
+			"fav_rooms": {
+			  "count": 0,
+			  "data": []
+			},
+			"recent_rooms": {
+			  "count": 0,
+			  "data": []
+			}
+		  },
+		  "roomID": "66bb6bf7-25be-45af-bc38-7e7e258797b8",
+		  "participant_count": 0,
+		  "room_name": "chill vibes",
+		  "description": "A room for relaxing and enjoying soothing music. Join us to unwind and chill with your favorite tunes.",
+		  "is_temporary": false,
+		  "is_private": false,
+		  "is_scheduled": false,
+		  "start_date": "2024-07-22T10:18:22.486Z",
+		  "end_date": "2024-07-22T10:18:22.486Z",
+		  "language": "English",
+		  "has_explicit_content": true,
+		  "has_nsfw_content": false,
+		  "room_image": "https://ik.imagekit.io/ikmedia/backlit.jpg",
+		  "current_song": {
+			"title": "",
+			"artists": [],
+			"cover": "",
+			"start_time": "2024-07-22T10:18:22.486Z"
+		  },
+		  "tags": [
+			"explicit"
+		  ]
+		}
+	  ];
+
+	beforeEach(async () => {
+		const module: TestingModule = await createSearchTestingModule();
+		service = module.get<SearchService>(SearchService);
+		dtoGen = module.get<DtoGenService>(DtoGenService);
+	});
+
+	it("should return an empty RoomDto array when query returns an empty array", async () => {
+		prismaMock.$queryRaw.mockResolvedValue([]);
+
+		const result = await service.searchRooms({q:"testing"});
+
+		expect(result).toMatchObject([new RoomDto()]);
+	});
+
+	it("should return a RoomDto array when query returns an array", async () => {
+		prismaMock.$queryRaw.mockResolvedValue(roomMock);
+		(dtoGen.generateMultipleRoomDto as jest.Mock).mockReturnValueOnce(rDtoMock);
+
+		const result = await service.searchRooms({q:"testing"});
+
+		expect(result).toMatchObject(rDtoMock);
 	});
 });
