27e83ee40c6784bd7aebfab9b4dc19a0a9867737
thabisomatau <u22609254@tuks.co.za>
2024-07-22 11:47:20 +0200

    fixed prisma schema

--- frontend/app/services/Utils.tsx
+++ frontend/app/services/Utils.tsx
@@ -1,21 +1,22 @@
 import localhost from "react-native-localhost";
 
 const USE_LOCAL_BACKEND_SERVER = true;
+const _localhost = "10.32.211.255";
 
 function getAPIBase(): string {
 	if (!USE_LOCAL_BACKEND_SERVER) {
 		//do something
 	}
-	console.log("Local IP Address: ", localhost);
-	return `http://${localhost}:3000`;
+	console.log("Local IP Address: ", _localhost);
+	return `http://${_localhost}:3000`;
 }
 
 function getLocalhost(): string {
 	if (!USE_LOCAL_BACKEND_SERVER) {
 		//do something
 	}
-	console.log("Local IP Address: ", localhost);
-	return `http://${localhost}`;
+	console.log("Local IP Address: ", _localhost);
+	return `http://${_localhost}`;
 }
 export const API_BASE_URL = getAPIBase();
 export const LOCALHOST = getLocalhost();
