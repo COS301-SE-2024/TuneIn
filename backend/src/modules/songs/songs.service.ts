07b9a03b5e91c9016f76d9510f4bdfebe99d1fa6
lkekana <theoriginalles@gmail.com>
2024-07-24 13:58:24 +0200

    implement getting spotifyID from song ID

--- backend/src/modules/songs/songs.service.ts
+++ backend/src/modules/songs/songs.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "prisma/prisma.service";
+import * as PrismaTypes from "@prisma/client";
 
 @Injectable()
 export class SongsService {
 	constructor(private readonly prisma: PrismaService) {}
+
+	async getSpotifyId(songID: string): Promise<{ id: string }> {
+		const song: PrismaTypes.song | null = await this.prisma.song.findUnique({
+			where: {
+				song_id: songID,
+			},
+		});
+		if (!song || !song.spotify_id) {
+			return { id: "null" };
+		}
+		return { id: song.spotify_id };
+	}
 }
