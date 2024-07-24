a241b9db6c00aa69af529818767a371f73a7d675
lkekana <theoriginalles@gmail.com>
2024-07-24 13:12:13 +0200

    prepare for fine-grained sync (unused)

--- backend/src/live/live.service.ts
+++ backend/src/live/live.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 import { Server, Socket } from "socket.io";
 
 @Injectable()
@@ -12,4 +13,24 @@ export class LiveService {
 	setServer(server: Server): void {
 		this.server = server;
 	}
+
+	//@Cron("*/5 * * * * *") // Run this every 5 seconds
+	/*
+	synchronizePlayback(): void {
+		const currentTime = Date.now(); // Get current Epoch time
+
+		for (const sessionId in this.queues) {
+			const queue = this.queues[sessionId];
+			const currentMedia = queue.find((item) => currentTime >= item.startTime);
+
+			if (currentMedia) {
+				// Broadcast current playback state to all clients in the session
+				this.server.to(sessionId).emit("syncPlayback", {
+					mediaId: currentMedia.mediaId,
+					position: currentTime - currentMedia.startTime,
+				});
+			}
+		}
+	}
+	*/
 }
