fa409df85762d0ad2baf4e5d6ad63a2bbb8debf4
Nerina <u21537144@tuks.co.za>
2024-08-02 20:28:43 +0200

    Analytics pages

---
+++ frontend/app/screens/analytics/InteractionsAnalytics.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	ScrollView,
+	StyleSheet,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+const LoginScreen: React.FC = () => {
+	const router = useRouter();
+
+	return (
+		<ScrollView style={styles.container}>
+			<TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+				<Ionicons name="chevron-back" size={30} color="black" />
+			</TouchableOpacity>
+			<View style={styles.logoContainer}>
+				{/* <Text style={styles.logoText}>Logo</Text> */}
+			</View>
+			<Text style={styles.headerText}>User Interactions in Room</Text>
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 16,
+	},
+	backButton: {
+		position: "absolute",
+		top: 16,
+		left: 16,
+		zIndex: 10,
+	},
+	logoContainer: {
+		alignItems: "center",
+		marginBottom: 40,
+	},
+	headerText: {
+		padding: 20,
+		fontSize: 32,
+		fontWeight: "bold",
+		textAlign: "center",
+		marginBottom: 40,
+	},
+});
+
+export default LoginScreen;
