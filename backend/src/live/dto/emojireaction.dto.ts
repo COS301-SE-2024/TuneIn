2b7836cb1aeb48fbd04a2bb4f0199b913b998d5e
lkekana <theoriginalles@gmail.com>
2024-08-02 12:42:39 +0200

    define EmojiDtos based on rn-emoji-picker
    from https://github.com/woodybury/rn-emoji-picker

---
+++ backend/src/live/dto/emojireaction.dto.ts
@@ -0,0 +1,40 @@
+//ChatEventDto
+// a class that defines the structure of the ChatEventDto object
+//the object is used to define the structure of the data that is sent & received to the server when a live chat event is triggered (websocket event)
+import { ApiProperty } from "@nestjs/swagger";
+import {
+	IsString,
+	IsDateString,
+	IsObject,
+	ValidateNested,
+} from "class-validator";
+
+export interface Emoji {
+	category: string;
+	keywords: string[];
+	name: string;
+	order: number;
+	unified: string;
+	emoji: string;
+}
+
+export interface Category {
+	key: string;
+	name: string;
+	emoji?: string;
+}
+
+export class EmojiReactionDto {
+	@ApiProperty()
+	@IsDateString()
+	date_created: Date;
+
+	@ApiProperty()
+	@IsObject()
+	@ValidateNested()
+	body: Emoji;
+
+	@ApiProperty()
+	@IsString()
+	userID: string;
+}
