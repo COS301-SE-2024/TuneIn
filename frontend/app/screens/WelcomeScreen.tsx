cd446e1983d804bb63c2e3563ccd05d9c1c1f0f4
Nerina <u21537144@tuks.co.za>
2024-08-11 23:52:57 +0200

    different logos added

--- frontend/app/screens/WelcomeScreen.tsx
+++ frontend/app/screens/WelcomeScreen.tsx
@@ -31,23 +31,25 @@ const WelcomeScreen: React.FC = () => {
 
 	return (
 		<View style={styles.container}>
-			<ImageBackground
-				source={require("../../assets/text.jpg")}
-				style={[styles.imageBackground, { width, height: height * 0.5 }]}
-				resizeMode="cover"
-			>
-				<TouchableOpacity style={styles.helpButton} onPress={navigateToHelp}>
-					<MaterialCommunityIcons
-						name="help-circle-outline"
-						size={24}
-						color="#FFF"
-						style={styles.helpIcon}
-					/>
-				</TouchableOpacity>
-			</ImageBackground>
+			<view style={styles.imageContainer}>
+				<ImageBackground
+					source={require("../../assets/logo4.png")}
+					style={[styles.imageBackground, { width, height: height * 0.6 }]}
+					resizeMode="cover"
+				>
+					<TouchableOpacity style={styles.helpButton} onPress={navigateToHelp}>
+						<MaterialCommunityIcons
+							name="help-circle-outline"
+							size={24}
+							color="#FFF"
+							style={styles.helpIcon}
+						/>
+					</TouchableOpacity>
+				</ImageBackground>
+			</view>
 			<View style={styles.innerContainer}>
-				<Text style={styles.logoText}>Logo</Text>
-				<Text style={styles.titleText}>TuneIn</Text>
+				{/* <Text style={styles.logoText}>Logo</Text>
+				<Text style={styles.titleText}>TuneIn</Text> */}
 				<CyanButton title="Login" onPress={navigateToLogin} />
 				<WhiteButton title="Register" onPress={navigateToRegister} />
 			</View>
@@ -60,10 +62,16 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 	},
+	imageContainer: {
+		paddingTop: 40,
+		flex: 1,
+		justifyContent: "center",
+		// paddingBottom: -20,
+	},
 	imageBackground: {
 		width: "100%",
 		height: "50%",
-		position: "relative",
+		// position: "relative",
 	},
 	innerContainer: {
 		flex: 1,
