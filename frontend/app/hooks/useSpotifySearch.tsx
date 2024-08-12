859190c33207dc94e0e675658ed6a98455c04468
lkekana <theoriginalles@gmail.com>
2024-08-12 10:03:20 +0200

    add typing to useSpotifySearch hook

--- frontend/app/hooks/useSpotifySearch.tsx
+++ frontend/app/hooks/useSpotifySearch.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import * as spotifyAuth from "../services/SpotifyAuth";
+import * as Spotify from "@spotify/web-api-ts-sdk";
 
-export const useSpotifySearch = () => {
-	const [searchResults, setSearchResults] = useState<any[]>([]);
+export const useSpotifySearch = (): {
+	searchResults: Spotify.Track[];
+	handleSearch: (query: string) => Promise<void>;
+	error: string | null;
+} => {
+	const [searchResults, setSearchResults] = useState<Spotify.Track[]>([]);
 	const [error, setError] = useState<string | null>(null);
-	const handleSearch = async (query: string) => {
+	const handleSearch = async (query: string): Promise<void> => {
 		try {
 			console.log("handleSearch: ", query);
 			const allTokens = await spotifyAuth.getTokens();
@@ -29,7 +34,9 @@ export const useSpotifySearch = () => {
 				throw new Error(`HTTP error! Status: ${response.status}`);
 			}
 
-			const data = await response.json();
+			const data = (await response.json()) as Required<
+				Pick<Spotify.PartialSearchResult, "tracks">
+			>;
 			setSearchResults(data.tracks.items);
 		} catch (err) {
 			setError("An error occurred while searching");
