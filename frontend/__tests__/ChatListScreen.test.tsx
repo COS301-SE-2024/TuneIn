9385f55acbd22f396c375ef9d6f9f99d238e4a62
lkekana <theoriginalles@gmail.com>
2024-08-18 16:17:59 +0200

    move mock dms to unit test

--- frontend/__tests__/ChatListScreen.test.tsx
+++ frontend/__tests__/ChatListScreen.test.tsx
@@ -20,6 +20,128 @@ jest.mock("expo-router", () => ({
 	useRouter: jest.fn(),
 }));
 
+const mockSelf: UserDto = {
+	userID: "1",
+	username: "john_doe",
+	profile_name: "John Doe",
+	profile_picture_url:
+		"https://images.pexels.com/photos/3792581/pexels-photo-3792581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+	followers: {
+		count: 0,
+		data: [],
+	},
+	following: {
+		count: 0,
+		data: [],
+	},
+	links: {
+		count: 0,
+		data: [],
+	},
+	bio: "",
+	current_song: {
+		title: "Song Name",
+		artists: ["Artist Name"],
+		cover: "",
+		start_time: new Date(),
+	},
+	fav_genres: {
+		count: 0,
+		data: [],
+	},
+	fav_songs: {
+		count: 0,
+		data: [],
+	},
+	fav_rooms: {
+		count: 0,
+		data: [],
+	},
+	recent_rooms: {
+		count: 0,
+		data: [],
+	},
+};
+
+const mockOtherUser: UserDto = {
+	profile_name: "",
+	userID: "",
+	username: "",
+	profile_picture_url: "",
+	followers: {
+		count: 0,
+		data: [],
+	},
+	following: {
+		count: 0,
+		data: [],
+	},
+	links: {
+		count: 0,
+		data: [],
+	},
+	bio: "",
+	current_song: {
+		title: "",
+		artists: [],
+		cover: "",
+		start_time: new Date(),
+	},
+	fav_genres: {
+		count: 0,
+		data: [],
+	},
+	fav_songs: {
+		count: 0,
+		data: [],
+	},
+	fav_rooms: {
+		count: 0,
+		data: [],
+	},
+	recent_rooms: {
+		count: 0,
+		data: [],
+	},
+	friendship: undefined,
+};
+
+const initialMessages: DirectMessageDto[] = [
+	{
+		index: 0,
+		messageBody: "Hey there!",
+		sender: mockSelf,
+		recipient: mockOtherUser,
+		dateSent: new Date(),
+		dateRead: new Date(0),
+		isRead: false,
+		pID: "0",
+	},
+	{
+		index: 1,
+		messageBody: "What's up?",
+		sender: mockOtherUser,
+		recipient: mockSelf,
+		dateSent: new Date(),
+		dateRead: new Date(0),
+		isRead: false,
+		pID: "1",
+	},
+	// Add more dummy messages
+];
+
+jest.mock("axios");
+(axios.get as jest.Mock).mockImplementation((url: string) => {
+	if (url.endsWith("users/dms")) {
+		return Promise.resolve({ data: initialMessages });
+	} else if (url.endsWith("users/friends")) {
+		return Promise.resolve({ data: [mockOtherUser] });
+	} else if (url.endsWith("users")) {
+		return Promise.resolve({ data: mockSelf });
+	}
+	return Promise.reject(new Error("Invalid URL"));
+});
+
 describe("ChatListScreen", () => {
 	const mockRouter = {
 		push: jest.fn(),
