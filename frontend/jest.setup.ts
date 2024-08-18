006cf1b0ba7560411df8198a66d7d3dc5b15b927
lkekana <theoriginalles@gmail.com>
2024-08-18 16:33:20 +0200

    implement other needed global mocks

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -6,6 +6,36 @@ import JWT from "expo-jwt";
 
 dotenv.config({ path: ".env" });
 jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
+global.alert = jest.fn();
+
+// Mocking StorageService module functions
+jest.mock("./app/services/StorageService", () => ({
+	setItem: jest.fn().mockResolvedValue(undefined),
+
+	// return a mock value unless the input is "token"
+	getItem: jest.fn().mockImplementation((key: string) => {
+		if (key === "token") {
+			return JWT.encode(
+				{
+					sub: "123456789",
+					aud: "123456789",
+					exp: Number.MAX_SAFE_INTEGER,
+					nbf: Number.MAX_SAFE_INTEGER,
+					iat: Number.MAX_SAFE_INTEGER,
+					jti: "123456789",
+				},
+				"mock-secret-key",
+			);
+		}
+		return jest.fn().mockResolvedValue(undefined);
+	}),
+	removeItem: jest.fn().mockResolvedValue(undefined),
+	clear: jest
+		.fn()
+		.mockResolvedValue(undefined)
+		.mockImplementation(() => console.log("Mock storage cleared")),
+}));
+
 jest.mock("socket.io-client", () => {
 	return {
 		io: jest.fn(() => ({
