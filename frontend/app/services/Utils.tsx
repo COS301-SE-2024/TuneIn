ace5cc2343ee840a2dca1a5ecb83379ba52aba4e
lkekana <theoriginalles@gmail.com>
2024-08-18 17:15:51 +0200

    undo-ing unnecessary changes that were on develop

--- frontend/app/services/Utils.tsx
+++ frontend/app/services/Utils.tsx
@@ -2,14 +2,12 @@ import localhost from "react-native-localhost";
 import { Buffer } from "buffer";
 
 const USE_LOCAL_BACKEND_SERVER = true;
-const _localhost: string = "10.32.211.255";
-
 function getAPIBase(): string {
 	if (!USE_LOCAL_BACKEND_SERVER) {
 		//do something
 	}
 	console.log("Local IP Address: ", localhost);
-	return `http://${_localhost}:3000`;
+	return `http://${localhost}:3000`;
 }
 
 function getLocalhost(): string {
@@ -17,7 +15,7 @@ function getLocalhost(): string {
 		//do something
 	}
 	console.log("Local IP Address: ", localhost);
-	return `http://${_localhost}`;
+	return `http://${localhost}`;
 }
 export const API_BASE_URL = getAPIBase();
 export const LOCALHOST = getLocalhost();
