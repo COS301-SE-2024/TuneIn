860aab27917fe979ac7974b8f0e4ec47868e8538
lkekana <theoriginalles@gmail.com>
2024-08-23 09:42:38 +0200

    added request logging with Morgan

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 import { MyLogger } from "./logger/logger.service";
+import * as morgan from "morgan";
 
 declare const module: any;
 
 async function bootstrap() {
+	const logger: MyLogger = new MyLogger();
 	const app = await NestFactory.create(AppModule, {
-		logger: new MyLogger(), // Use custom logger
+		logger: logger, // Use custom logger
 	});
 
 	// Validation
@@ -34,6 +36,12 @@ async function bootstrap() {
 		module.hot.accept();
 		module.hot.dispose(() => app.close());
 	}
+
+	app.use(morgan('combined', {
+		stream: {
+		write: (message: string) => logger.log(message.trim()),
+		},
+	}));
 
 	await app.listen(3000);
 }
