3071612b2bd0bf2c3372b5bc52b0cac810470a83
lkekana <theoriginalles@gmail.com>
2024-08-25 14:42:23 +0200

    use start script to start backend (& ensure we use dev branch for deployment)

---
+++ backend/start_backend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Path to the main entry file
+MAIN_FILE="./dist/src/main.js"
+
+# Check if the main file exists
+if [ ! -f "$MAIN_FILE" ]; then
+    echo "Error: $MAIN_FILE not found. Build might have failed. Going to retry..."
+    cd backend
+    nvm use default
+    npm i
+    npm run build:swc
+fi
+
+if [ ! -f "$MAIN_FILE" ]; then
+  echo "Error: $MAIN_FILE not found. Build might have failed. Exiting..."
+  exit 1
+fi
+
+# Start the application with PM2
+pm2 start ecosystem.config.js
