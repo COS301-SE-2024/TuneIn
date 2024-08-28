750bb9fa661d06600d9d3afffea420975f19b769
thabisomatau <u22609254@tuks.co.za>
2024-08-28 20:10:41 +0200

    code cleanup

--- backend/src/recommender/recommender.service.ts
+++ backend/src/recommender/recommender.service.ts
@@ -1,9 +1,7 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import * as fs from "fs";
-import * as path from "path";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class RecommenderService implements OnModuleInit {
+export class RecommenderService {
 	private featureWeights = {
 		danceability: 1,
 		energy: 1,
@@ -19,48 +17,26 @@ export class RecommenderService implements OnModuleInit {
 	};
 
 	private playlists: { [key: string]: any[] } = {};
-	private mockSongs = [
-		{
-			danceability: 0.626,
-			energy: 0.67,
-			key: 7,
-			loudness: -6.6,
-			mode: 0,
-			speechiness: 0.0944,
-			acousticness: 0.55,
-			instrumentalness: 0,
-			liveness: 0.116,
-			valence: 0.497,
-			tempo: 79.839,
-		},
-	];
+	private mockSongs: {
+		danceability: number;
+		energy: number;
+		key: number;
+		loudness: number;
+		mode: number;
+		speechiness: number;
+		acousticness: number;
+		instrumentalness: number;
+		liveness: number;
+		valence: number;
+		tempo: number;
+	}[] = [];
 
-	// constructor() {}
-	// constructor that takes in playlists and favoriteSongs
 	constructor() {}
 	setPlaylists(playlists: { [key: string]: any[] }) {
 		this.playlists = playlists;
 	}
 	setMockSongs(mockSongs: any[]) {
 		this.mockSongs = mockSongs;
-	}
-	async onModuleInit() {
-		await this.loadData();
-	}
-
-	private async loadData(): Promise<void> {
-		const filePath = path.join(__dirname, "..", "combined_playlists.json"); // this is path to the mock data JSON
-
-		try {
-			const data = fs.readFileSync(filePath, "utf-8");
-			const parsedData = JSON.parse(data);
-			this.playlists = parsedData.playlists
-				? this.flattenPlaylists(parsedData.playlists)
-				: {};
-			console.log("Data loaded successfully");
-		} catch (error) {
-			console.error("Error loading JSON file:", error);
-		}
 	}
 
 	private flattenPlaylists(playlists: { [key: string]: any[] }): {
@@ -104,31 +80,6 @@ export class RecommenderService implements OnModuleInit {
 
 			weightedSimilarities += dotProduct / (magnitude1 * magnitude2);
 		}
-		// const dotProduct = Object.keys(song1).reduce((sum, key) => {
-		// 	if (key in weights) {
-		// 		return sum + song1[key] * song2[key] * weights[key];
-		// 	}
-		// 	return sum;
-		// }, 0);
-
-		// const magnitude1 = Math.sqrt(
-		// 	Object.keys(song1).reduce((sum, key) => {
-		// 		if (key in weights) {
-		// 			return sum + song1[key] * song1[key] * weights[key];
-		// 		}
-		// 		return sum;
-		// 	}, 0),
-		// );
-
-		// const magnitude2 = Math.sqrt(
-		// 	Object.keys(song2).reduce((sum, key) => {
-		// 		if (key in weights) {
-		// 			return sum + song2[key] * song2[key] * weights[key];
-		// 		}
-		// 		return sum;
-		// 	}, 0),
-		// );
-
 		return weightedSimilarities / favoriteSongs.length;
 	}
 
@@ -156,18 +107,9 @@ export class RecommenderService implements OnModuleInit {
 		return playlistScores;
 	}
 	analyzeFeatureDistribution = (playlists: any[]) => {
-		// console.log("Analyzing feature distribution");
-		// console.log(playlists);
 		const features = ["danceability", "energy", "loudness", "tempo", "valence"];
 		features.forEach((feature) => {
-			// console.log(
-			// 	"Playlist map: ",
-			// 	playlists.map((p) => p[feature]),
-			// 	"with feature: ",
-			// 	feature,
-			// );
 			const values = playlists.map((p) => {
-				// console.log("p: ", p[0][feature]);
 				return p[0][feature];
 			});
 			const min = Math.min(...values);
