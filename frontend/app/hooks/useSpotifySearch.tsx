60ae99b1350253fcaaf6d59f717e35b69f4cbef4
lkekana <u20486473@tuks.co.za>
2024-09-23 20:03:59 +0200

    fix new search

--- frontend/app/hooks/useSpotifySearch.tsx
+++ frontend/app/hooks/useSpotifySearch.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import * as Spotify from "@spotify/web-api-ts-sdk";
 import { useLive } from "../LiveContext";
 
@@ -11,19 +11,23 @@ export const useSpotifySearch = (): {
 	const [searchResults, setSearchResults] = useState<Spotify.Track[]>([]);
 	const [error, setError] = useState<string | null>(null);
 
-	const handleSearch = async (query: string): Promise<void> => {
-		try {
-			await spotifyAuth.userlessAPI
-				.search(query, ["track"])
-				.then((response) => {
-					console.log("response", response);
-					setSearchResults(response.tracks.items);
-				});
-		} catch (err) {
-			setError("An error occurred while searching");
-			console.error("Error performing Spotify search: ", err);
-		}
-	};
+	const handleSearch = useCallback(
+		async (query: string): Promise<void> => {
+			try {
+				await spotifyAuth.userlessAPI
+					.search(query, ["track"])
+					.then((response) => {
+						console.log("search results");
+						console.log(response);
+						setSearchResults(response.tracks.items);
+					});
+			} catch (err) {
+				setError("An error occurred while searching");
+				console.error("Error performing Spotify search: ", err);
+			}
+		},
+		[spotifyAuth.userlessAPI],
+	);
 
 	return {
 		searchResults,
