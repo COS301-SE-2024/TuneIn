9ffd975186341b5b221cb7b96c3d7711406cab50
lkekana <u20486473@tuks.co.za>
2024-09-24 15:28:52 +0200

    fix getting liked songs

--- backend/src/spotify/spotify.service.ts
+++ backend/src/spotify/spotify.service.ts
@@ -159,198 +159,27 @@ export class SpotifyService {
 		if (new Date().getTime() > tk.epoch_expiry) {
 			throw new Error("Token has expired");
 		}
-
 		const api = SpotifyApi.withAccessToken(this.clientId, tk.tokens);
-		let total = Number.MAX_SAFE_INTEGER;
-		let retrieved = 0;
-		const likedSongs: Spotify.SavedTrack[] = [];
-		while (retrieved < total) {
-			console.log(retrieved + " / " + total + " liked songs");
-			for (let i = 0; i < NUMBER_OF_RETRIES; i++) {
-				try {
-					const tracks = await api.currentUser.tracks.savedTracks(
-						50,
-						retrieved,
-					);
-					if (total === Number.MAX_SAFE_INTEGER) {
-						total = tracks.total;
-					}
-					likedSongs.push(...tracks.items);
-					retrieved += tracks.items.length;
-					break;
-				} catch (e) {
-					console.error(e);
-				}
-				console.log("Retrying...");
-			}
-			await sleep(1000);
+		const initialSongsFetch: Spotify.Page<Spotify.SavedTrack> =
+			await api.currentUser.tracks.savedTracks();
+		const total = initialSongsFetch.total;
+		let i = 0;
+		const promises: Promise<Spotify.Page<Spotify.SavedTrack>>[] = [];
+		while (i < total) {
+			const p = api.currentUser.tracks.savedTracks(50, i);
+			promises.push(p);
+			i += 50;
+			await this.wait(500); // for rate limiting
 		}
 
-		//dedupe
-		console.log("Deduping liked songs");
-		const seen = new Set();
-		const likedSongsDeduped = likedSongs.filter((el) => {
-			const duplicate = seen.has(el.track.id);
-			seen.add(el.track.id);
-			return !duplicate;
-		});
-		return likedSongsDeduped;
-	}
-
-	async importUserLibrary(tk: SpotifyTokenPair, userID: string): Promise<void> {
-		if (new Date().getTime() > tk.epoch_expiry) {
-			throw new Error("Token has expired");
-		}
-
-		const api = SpotifyApi.withAccessToken(this.clientId, tk.tokens);
-
-		const playlists: Spotify.SimplifiedPlaylist[] = await this.getUserPlaylists(
-			tk,
+		const songs: Spotify.Page<Spotify.SavedTrack>[] = await Promise.all(
+			promises,
 		);
-		const likedSongs: Spotify.SavedTrack[] = await this.getAllLikedSongs(tk);
-
-		const foundSongs: Spotify.PlaylistedTrack[] = [];
-		for (const playlist of playlists) {
-			const fullPlaylist: Spotify.Playlist = await api.playlists.getPlaylist(
-				playlist.id,
-			);
-
-			//get all song ids in the playlist
-			const playlistSongIds: string[] = fullPlaylist.tracks.items.map(
-				(track) => track.track.id,
-			);
-
-			//find playlist songs that are in the database
-			const existingSongs: PrismaTypes.song[] | null =
-				await this.prisma.song.findMany({
-					where: {
-						spotify_id: {
-							in: playlistSongIds,
-						},
-					},
-				});
-			if (!existingSongs || existingSongs === null) {
-				throw new Error("Failed to find songs");
-			}
-
-			//find songs that are not in the database
-			const newSongIds: string[] = playlistSongIds.filter((id) => {
-				return !existingSongs.some((song) => song.spotify_id === id);
-			});
-
-			//gather PlaylistedTrack objects for new songs
-			const newSongs: Spotify.PlaylistedTrack[] =
-				fullPlaylist.tracks.items.filter((track) => {
-					return newSongIds.includes(track.track.id);
-				});
-
-			foundSongs.push(...newSongs);
-			await sleep(1000);
+		const likedSongs: Spotify.SavedTrack[] = [];
+		for (const s of songs) {
+			likedSongs.push(...s.items);
 		}
-
-		//find liked songs already in db
-		let likedSongsInDB: PrismaTypes.song[] | null =
-			await this.prisma.song.findMany({
-				where: {
-					spotify_id: {
-						in: likedSongs.map((track) => track.track.id),
-					},
-				},
-			});
-		if (!likedSongsInDB || likedSongsInDB === null) {
-			throw new Error("Failed to find songs");
-		}
-
-		const ls: PrismaTypes.song[] = likedSongsInDB;
-		//collect liked songs not in db
-		const newLikedSongs: Spotify.SavedTrack[] = likedSongs.filter((track) => {
-			return !ls.some((song) => song.spotify_id === track.track.id);
-		});
-
-		const dbLikedSongs: Prisma.songCreateInput[] = [];
-		for (const track of newLikedSongs) {
-			// const audioFeatures: Spotify.AudioFeatures =
-			// 	await api.tracks.audioFeatures(track.track.id);
-			const song: Prisma.songCreateInput = {
-				name: track.track.name,
-				duration: track.track.duration_ms,
-				artists: track.track.artists.map((artist) => artist.name),
-				genres: track.track.album.genres ? track.track.album.genres : [],
-				artwork_url: this.getLargestImage(track.track.album.images).url,
-				spotify_id: track.track.id,
-				// audio_features: JSON.stringify(audioFeatures),
-				audio_features: {},
-			};
-			dbLikedSongs.push(song);
-		}
-
-		const newSongs: PrismaTypes.song[] =
-			await this.prisma.song.createManyAndReturn({
-				data: dbLikedSongs,
-				skipDuplicates: true,
-			});
-		console.log("Created " + newSongs.length + " new songs");
-
-		let allIDs: string[] = likedSongs.map((track) => track.track.id);
-		likedSongsInDB = await this.prisma.song.findMany({
-			where: {
-				spotify_id: {
-					in: allIDs,
-				},
-			},
-		});
-		allIDs = likedSongsInDB.map((song) => song.song_id);
-
-		const likedSongsPlaylist: PrismaTypes.playlist | null =
-			await this.prisma.playlist.findFirst({
-				where: {
-					name: "Liked Songs (generated by TuneIn)",
-					user_id: userID,
-				},
-			});
-		if (likedSongsPlaylist) {
-			await this.prisma.playlist.update({
-				where: {
-					playlist_id: likedSongsPlaylist.playlist_id,
-				},
-				data: {
-					playlist: allIDs,
-				},
-			});
-		} else {
-			const likedSongsPlaylist: Prisma.playlistCreateInput = {
-				name: "Liked Songs (generated by TuneIn)",
-				users: {
-					connect: { user_id: userID },
-				},
-				playlist: allIDs,
-			};
-			await this.prisma.playlist.create({
-				data: likedSongsPlaylist,
-			});
-		}
-	}
-
-	async findSavedSongInDB(
-		song: Spotify.SavedTrack,
-	): Promise<PrismaTypes.song | null> {
-		const search = await this.prisma.song.findFirst({
-			where: {
-				spotify_id: song.track.id,
-			},
-		});
-		return search;
-	}
-
-	async findPlaylistSongInDB(
-		song: Spotify.PlaylistedTrack,
-	): Promise<PrismaTypes.song | null> {
-		const search = await this.prisma.song.findFirst({
-			where: {
-				spotify_id: song.track.id,
-			},
-		});
-		return search;
+		return likedSongs;
 	}
 
 	getLargestImage(images: Spotify.Image[]): Spotify.Image {
