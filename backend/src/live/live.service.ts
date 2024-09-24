befcafbbd50b2929c53c1dcc17ef4662aecfe9ad
lkekana <u20486473@tuks.co.za>
2024-09-24 17:42:31 +0200

    implement scheduled playback announcements

--- backend/src/live/live.service.ts
+++ backend/src/live/live.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from "@nestjs/common";
-//import { Cron } from "@nestjs/schedule";
+import { Cron, SchedulerRegistry } from "@nestjs/schedule";
+import { CronJob } from "cron";
 import { Server } from "socket.io";
 
 @Injectable()
 export class LiveService {
 	private server: Server;
+	private roomJobs: Map<string, CronJob[]>;
+	constructor(
+		private readonly schedulerRegistry: SchedulerRegistry,
+		private readonly roomQueue: RoomQueueService,
+	) {
+		this.roomJobs = new Map<string, CronJob[]>();
+	}
+
+	async wait(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 
 	serverSet(): boolean {
 		return this.server !== undefined;
@@ -29,8 +41,58 @@ export class LiveService {
 					mediaId: currentMedia.mediaId,
 					position: currentTime - currentMedia.startTime,
 				});
+	createSongAnnouncement(
+		roomID: string,
+		startTime: Date,
+		event: PlaybackEventDto,
+	): void {
+		if (!this.serverSet()) {
+			throw new Error("Server not set");
+		}
+		console.log(
+			`Creating new song announcement for room ${roomID} at ${startTime.toISOString()}`,
+		);
+		const job = new CronJob(startTime, async () => {
+			try {
+				console.log(`${roomID}: New song announcement for room`);
+				if (event.song && event.song.track) {
+					console.log(
+						`${roomID}: Play song (index=${event.song.index}): ${event.song.spotifyID} ${event.song.track.name} by ${event.song.track.artists[0].name}`,
+					);
+				}
+				for (let i = 0; i < 5; i++) {
+					this.server.to(roomID).emit(SOCKET_EVENTS.CURRENT_MEDIA, event);
+					console.log(`${roomID}: Sent announcement ${i + 1} of 5`);
+					await this.wait(1000);
+				}
+				console.log(`${roomID}: New song announcement complete.`);
+			} catch (err) {
+				console.error(`${roomID}: Error sending song announcement`);
+				console.error(err);
 			}
+		});
+		this.schedulerRegistry.addCronJob(
+			`songAnnouncement-${roomID}-${startTime.valueOf()}`,
+			job,
+		);
+		if (!this.roomJobs.has(roomID)) {
+			this.roomJobs.set(roomID, []);
+		}
+		const queue = this.roomJobs.get(roomID);
+		if (queue) queue.push(job);
+		job.start();
+		console.log(
+			`Successfully created announcement for room ${roomID} at ${startTime.toISOString()}`,
+		);
+	}
+
+	cancelSongAnnouncements(roomID: string): void {
+		const jobs = this.roomJobs.get(roomID);
+		if (jobs) {
+			for (const job of jobs) {
+				job.stop();
+			}
+			this.roomJobs.delete(roomID);
 		}
 	}
-	*/
 }
