0ceb4374361cfaad478af25172ca3d444a213a5c
Jaden Moodley <u22528492@tuks.co.za>
2024-09-26 22:29:29 +0200

    Changed auth setup on frontend

--- frontend/app/screens/WelcomeScreen.tsx
+++ frontend/app/screens/WelcomeScreen.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import {
+	Text,
 	View,
 	ImageBackground,
 	TouchableOpacity,
 	Dimensions,
 	StyleSheet,
+	ToastAndroid,
 } from "react-native";
 import { useRouter } from "expo-router";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as utils from "../services/Utils";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import CyanButton from "../components/CyanButton";
 import WhiteButton from "../components//WhiteButton";
+import { DiscoveryDocument, makeRedirectUri, ResponseType, useAuthRequest } from "expo-auth-session";
+import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "react-native-dotenv";
+import { generateRandom } from "expo-auth-session/build/PKCE";
+import {
+	exchangeCodeWithBackend,
+	SpotifyCallbackResponse,
+} from "../services/SpotifyAuth";
+import { live } from "../services/Live";
+import auth from "../services/AuthManagement";
+import { colors } from "../styles/colors";
+
 
 const WelcomeScreen: React.FC = () => {
 	const router = useRouter();
@@ -26,6 +40,96 @@ const WelcomeScreen: React.FC = () => {
 	const navigateToHelp = () => {
 		router.navigate("/screens/(tabs)/HelpScreenelpScreen");
 	};
+
+	const redirectURI = makeRedirectUri({
+		scheme: "tunein-app",
+		path: "screens/Auth/SpotifyRedirect",
+		native: "tunein-app://screens/Auth/SpotifyRedirect", // Use the 'native' option for standalone or native contexts
+		preferLocalhost: true,
+	});
+
+	const makeStateVariable = (redirectURI: string) => {
+		const state = {
+			"unique-pre-padding": generateRandom(10),
+			"expo-redirect": redirectURI,
+			"ip-address": utils.API_BASE_NO_PORT,
+			"redirect-used": SPOTIFY_REDIRECT_URI,
+			"unique-post-padding": generateRandom(10),
+		};
+		const bytes = new TextEncoder().encode(JSON.stringify(state));
+		const b64 = utils.bytesToBase64(bytes);
+		return b64;
+	};
+
+	const scopes = [
+		"user-read-email",
+		"user-library-read",
+		"user-read-recently-played",
+		"user-top-read",
+		"playlist-read-private",
+		"playlist-read-collaborative",
+		"playlist-modify-public", // or "playlist-modify-private"
+		"user-modify-playback-state",
+		"user-read-playback-state",
+		"user-read-currently-playing",
+	];
+
+	const discovery: DiscoveryDocument = {
+		authorizationEndpoint: "https://accounts.spotify.com/authorize",
+	};
+
+	const state = React.useMemo(
+		() => makeStateVariable(redirectURI),
+		[redirectURI],
+	);
+
+	
+
+	const [request, response, promptAsync] = useAuthRequest(
+		{
+			clientId: SPOTIFY_CLIENT_ID,
+			//redirectUri: SPOTIFY_REDIRECT_URI,
+			redirectUri: redirectURI,
+			responseType: ResponseType.Code,
+			scopes: [scopes.join(" ")],
+			usePKCE: false,
+			state: state,
+		},
+		discovery,
+	);
+
+	React.useEffect(() => {
+		console.log("Response:", response);
+		if (response && response !== null) {
+			if (response?.type === "success") {
+				if (response.params.error) {
+					console.error("Error:", response.params.error);
+					return;
+				}
+				const { code, state } = response.params;
+				console.log("Code:", code);
+
+				//make post request to backend server get access token
+				const doExchange = async () => {
+					const tokens: SpotifyCallbackResponse = await exchangeCodeWithBackend(
+						code,
+						state,
+						redirectURI,
+					);
+					await auth.setToken(tokens.token);
+					live.initialiseSocket();
+					router.navigate("screens/(tabs)/Home");
+				};
+				doExchange();
+			} else {
+				ToastAndroid.show("Failed to authenticate. Please try again.", ToastAndroid.SHORT);
+				// throw new Error(
+				// 	"Received unsuccessful response from Spotify. Please try again.",
+				// );
+			}
+		}
+	}, [response, redirectURI, router]);
+
 
 	return (
 		<View style={styles.container}>
@@ -50,10 +154,20 @@ const WelcomeScreen: React.FC = () => {
 				</ImageBackground>
 			</View>
 			<View style={styles.innerContainer}>
-				{/* <Text style={styles.logoText}>Logo</Text>
-				<Text style={styles.titleText}>TuneIn</Text> */}
-				<CyanButton title="Login" onPress={navigateToLogin} />
-				<WhiteButton title="Register" onPress={navigateToRegister} />
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => {
+						promptAsync();
+					}}
+				>
+					<FontAwesome
+						name="spotify"
+						size={24}
+						color="#000"
+						style={styles.icon}
+					/>
+					<Text style={styles.buttonText}>Login With Spotify</Text>
+				</TouchableOpacity>
 			</View>
 		</View>
 	);
@@ -99,6 +213,30 @@ const styles = StyleSheet.create({
 	},
 	helpIcon: {
 		marginRight: 5,
+	},
+	button: {
+		width: "92%",
+		height: 52,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: colors.primary,
+		borderRadius: 56,
+		marginBottom: 20,
+		flexDirection: "row",
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	buttonText: {
+		fontSize: 18,
+		fontWeight: "bold",
+		color: "#FFF",
+	},
+	icon: {
+		color: "white",
+		marginRight: 10,
 	},
 });
 
