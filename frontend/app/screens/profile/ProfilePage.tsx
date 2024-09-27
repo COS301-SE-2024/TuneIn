d6959cb4564622bcfb0089fc18798361a04552f8
Jaden Moodley <u22528492@tuks.co.za>
2024-09-28 00:02:14 +0200

    lint fix

--- frontend/app/screens/profile/ProfilePage.tsx
+++ frontend/app/screens/profile/ProfilePage.tsx
@@ -977,7 +977,7 @@ const ProfileScreen: React.FC = () => {
 					testID="refresh-control"
 				/>
 			}
-			style={{backgroundColor: colors.backgroundColor}}
+			style={{ backgroundColor: colors.backgroundColor }}
 		>
 			<View
 				style={{
