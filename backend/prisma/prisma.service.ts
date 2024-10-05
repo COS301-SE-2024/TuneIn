5be4cf3e8f95bbaa44e0f1d58c641be6763bce42
lkekana <theoriginalles@gmail.com>
2024-10-05 13:18:49 +0200

    handle Prisma connection retries

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -1,13 +1,60 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import {
+	Injectable,
+	OnModuleInit,
+	OnModuleDestroy,
+	Logger,
+} from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import pRetry from "p-retry";
+
+const RETRIES = 10;
 
 @Injectable()
 export class PrismaService
 	extends PrismaClient
 	implements OnModuleInit, OnModuleDestroy
 {
+	private readonly logger = new Logger(PrismaService.name);
+
+	constructor() {
+		super();
+		process.on("SIGINT", async () => {
+			this.logger.warn(
+				"SIGINT signal received. Closing database connection...",
+			);
+			await this.$disconnect();
+			process.exit(0);
+		});
+	}
+
 	async onModuleInit() {
-		await this.$connect();
+		try {
+			await pRetry(
+				async () => {
+					await this.$connect();
+					this.logger.log("Connected to the database");
+					return;
+				},
+				{
+					onFailedAttempt: (error) => {
+						// console.log(
+						// 	`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+						// );
+						this.logger.log(
+							`Database connection failed: ${error.message}. Retrying... (${
+								error.attemptNumber + 1
+							}/${RETRIES})`,
+						);
+					},
+					retries: RETRIES,
+				},
+			);
+		} catch (error) {
+			this.logger.error(
+				`Failed to connect to the database after ${RETRIES} retries. Exiting...`,
+			);
+			process.exit(1);
+		}
 	}
 
 	async onModuleDestroy() {
